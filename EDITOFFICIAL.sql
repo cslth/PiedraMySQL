97a76af0259ebc7c1d3aa6ff2fa6e7a6509de3b4
ChavaMS <51062516+ChavaMS@users.noreply.github.com>
2019-05-27 11:26:11 -0500

    Version actualizada

--- EDITOFFICIAL.sql
+++ EDITOFFICIAL.sql
@@ -1,11 +1,9 @@
--- DROP database editorialpiedra;
 CREATE DATABASE EditorialPiedra;
 USE EditorialPiedra;
 
 CREATE TABLE Sesiones(ses_id int(11) NOT NULL auto_increment primary key,
 					  ses_cadena char(32) NOT NULL,
                       ses_inicio datetime NOT NULL,
-                      ses_ultima_actividad datetime NOT NULL,
                       ses_fin datetime,
                       ses_ip varchar(15) NOT NULL,
                       ses_so varchar(255),
@@ -31,54 +29,39 @@ CREATE TABLE Roles_Usuarios(ru_id int(11) NOT NULL auto_increment primary key,
                             rol_id int(11) NOT NULL,
                             usr_id int(11) NOT NULL,
                             ses_id int(11) NOT NULL);
-                            
-INSERT INTO Sesiones(ses_cadena, ses_inicio, ses_ultima_actividad,ses_fin,ses_ip,ses_so,usr_id)
-VALUES('ses1',now(),now(),now(),'localhost','Windows 10',1);    
 
+-- Creacón de sesiones
+-- Sesion 1
+INSERT INTO Sesiones(ses_cadena, ses_inicio,ses_fin,ses_ip,ses_so,usr_id)
+VALUES('sesion1',now(),now(),'localhost','Windows 10',1); 
+
+-- Usuario 1
 INSERT INTO Usuarios(usr_login, usr_pwd, ses_id)
 VALUES('Administrator','hf6LJr9X$b9RMCJJ+gU5HpCqHh_XG6U_',1); 
 
-SELECT usr_login FROM Usuarios WHERE usr_login = 'Administrator';
-SELECT usr_pwd FROM Usuarios WHERE usr_login = 'Administrator';
-SELECT * FROM Usuarios;
-SELECT * FROM Sesiones;
+INSERT INTO Usuarios(usr_login, usr_pwd, ses_id)
+VALUES('Chava','12345',1); 
 
-ALTER TABLE Sesiones
-DROP ses_ultima_actividad;
-
-SELECT creado.usr_login AS 'Cuenta creada',	
-       creador.usr_login AS 'Creador' 
-       FROM usuarios AS creado
-       INNER JOIN sesiones AS ses ON(creado.ses_id = ses.ses_id)
-       INNER JOIN usuarios AS creador ON (ses.usr_id = creador.usr_id)
-       WHERE creador.usr_id = (SELECT creador.usr_id
-       FROM usuarios AS creado
-       INNER JOIN sesiones AS ses ON(creado.ses_id = ses.ses_id)
-       INNER JOIN usuarios AS creador ON (ses.usr_id = creador.usr_id)
-       GROUP BY creador.usr_login
-       ORDER BY COUNT(*) DESC
-       LIMIT 2,1);
-
-SELECT MAX(ses_id) AS SES_ID FROM Sesiones WHERE usr_id = (SELECT usr_id FROM Usuarios WHERE usr_login = 'Administrator');
-
+-- Rol
 INSERT INTO Roles(rol_nombre,ses_id) 
-VALUES('superUser',1);      
+VALUES('superUser',1);   
+ 
+INSERT INTO Roles(rol_nombre,ses_id) 
+VALUES('Empleado',1);    
 
+-- Rol_Usuario
 INSERT INTO Roles_Usuarios(ru_fecha,rol_id,usr_id,ses_id)
 VALUES(now(),1,1,1);
 
+INSERT INTO Roles_Usuarios(ru_fecha,rol_id,usr_id,ses_id)
+VALUES(now(),2,2,1);
+
+-- Permisos del Rol
 INSERT INTO Permisos(perm_permisos,perm_fcreacion,ses_id,rol_id)
 VALUES('FFFF',now(),1,1);       
 
-UPDATE Sesiones
-SET ses_fin = now()
-WHERE ses_id = 1
-and usr_id = 1;   
-
-UPDATE Sesiones
-SET ses_fin = null
-WHERE ses_id = 1
-and usr_id = 1;          
+INSERT INTO Permisos(perm_permisos,perm_fcreacion,ses_id,rol_id)
+VALUES('8008',now(),1,2);  
 
 CREATE TABLE Empleado (ID int(3) NOT NULL AUTO_INCREMENT, 
 					   Nombre varchar(255) NOT NULL,
@@ -92,10 +75,7 @@ CREATE TABLE Empleado (ID int(3) NOT NULL AUTO_INCREMENT,
 					   FechaNacimiento date NOT NULL,
 					   imagenEmpleado varchar(255)  NOT NULL, -- Tiene que ser null 
 					   Sexo bit(1) NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB;
-					
--- Select * from Empleado;
-                       
-
+									
 CREATE TABLE Telefonos (EmpleadoID int(3) NOT NULL, 
 	                    Telefono varchar(20) NOT NULL, 
 	                    PRIMARY KEY (Telefono)) ENGINE=InnoDB;
@@ -103,10 +83,8 @@ CREATE TABLE Telefonos (EmpleadoID int(3) NOT NULL,
 CREATE TABLE RedesSociales (EmpleadoID int(3) NOT NULL, 
 	                        Cuenta varchar(255) NOT NULL,
                             TipoDeRedSocialID int(10) NOT NULL,
-	                        PRIMARY KEY (TipoDeRedSocialID, Cuenta)) ENGINE=InnoDB;
-                            
--- DROP TABLE RedesSociales;                            
-                            
+	                        PRIMARY KEY (EmpleadoID, Cuenta,TipoDeRedSocialID)) ENGINE=InnoDB;
+                                                      
 CREATE TABLE TipoDeRedSocial (ID int(3) NOT NULL AUTO_INCREMENT,
 							  NombreRedSocial varchar(255) NOT NULL,
                               PRIMARY KEY (ID));                            
@@ -279,41 +257,8 @@ ALTER TABLE Pagos
 ADD CONSTRAINT FKPagos745505 
 FOREIGN KEY (MetodoID) REFERENCES Metodo (ID);
 
-  INSERT INTO Empleado (Nombre,email,Calle,Colonia,Ciudad,Estado,RFC,FechaNacimiento,imagenEmpleado,Sexo) 
-  VALUES ("Salvador Santos","chava@gmail.com","triana","triana", 
-  "Torreon","Coahuila","MESS981016",'1998/03/03', "En el disco local D ",1);
-  
-  INSERT INTO Empleado (Nombre,email,Calle,Colonia,CP,Ciudad,Estado,RFC,FechaNacimiento,imagenEmpleado,Sexo) 
-  VALUES ("Prueba","prueba@gmail.com","triana","triana",1234, 
-  "Torreon","Coahuila","MESS981016",'1998/03/03', "En el disco local D ",1);
-  
-  INSERT INTO Empleado (Nombre,email,Calle,Colonia,Ciudad,Estado,RFC,FechaNacimiento,imagenEmpleado,Sexo) 
-  VALUES ("Javier Eluney","eluney@gmail.com","Desierto de arizona 65-11","Santa elena", 
-  "Torreon","Coahuila","MESS981016",'1998/03/10', "En el disco local C ",0);
-  
-INSERT INTO Empleado (Nombre,email,Calle,Colonia,Ciudad,Estado,RFC,FechaNacimiento,imagenEmpleado,Sexo) 
-  VALUES ("Josue Romero","josue@gmail.com","Desierto de arabia 67-12","Santa elena", 
-  "Torreon","Coahuila","MESS981016",'1998/03/03', "En el disco local D ",1);
-  
- INSERT INTO Telefonos
-VALUES(1,'8713321074');  
-
-INSERT INTO Telefonos
-VALUES(1,'8713321045');  
-
-INSERT INTO Telefonos
-VALUES(1,'8713321099');
-
-INSERT INTO Telefonos
-VALUES(2,'8713321555');  
-
-INSERT INTO Telefonos
-VALUES(3,'56736576');
-
-INSERT INTO Telefonos
-VALUES((SELECT MAX(ID) from Empleado),'654565465'); 
-  
-  INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Diseñador','Diseña independientemente del trabajo');
+-- Inserts por defautl en la BD
+INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Diseñador','Diseña independientemente del trabajo');
 INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Editor','Edita independientemente del trabajo');   
 INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Fotografo','Toma fotos independientemente del trabajo');
 INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Maquetador','Hace maquetas independientemente del trabajo');
@@ -321,8 +266,8 @@ INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Ilustrador','Ilustra in
 INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Imprenta','Se encarga de la impresión independientemente del trabajo');   
 INSERT INTO TipoEmpleado(NombreTipo,Descripcion) VALUES('Publicidad','Se encarga de la publicidad independientemente del trabajo');  
 
-  
-  INSERT INTO TipoTrabajos (NombreTipoTrab,DescripcionTrab)
+
+INSERT INTO TipoTrabajos (NombreTipoTrab,DescripcionTrab)
 VALUES("Revision", "Revisa el formato completo de un escrito");
 INSERT INTO TipoTrabajos (NombreTipoTrab,DescripcionTrab)
 VALUES("Dibujo", "Revisa el formato completo de un escrito");
@@ -331,74 +276,24 @@ VALUES("Otro", "Revisa el formato completo de un escrito");
 
 INSERT INTO TipoDeRedSocial(NombreRedSocial)
 VALUES("Facebook");
-
 INSERT INTO TipoDeRedSocial(NombreRedSocial)
 VALUES("Instagram");
-
 INSERT INTO TipoDeRedSocial(NombreRedSocial)
 VALUES("Twitter");
 
-  
-  INSERT INTO Metodo(Metodo) VALUES ('Transferencia');
- INSERT INTO Metodo(Metodo) VALUES ('Cheque');
- INSERT INTO Metodo(Metodo) VALUES ('Efectivo');
+INSERT INTO Metodo(Metodo) VALUES ('Transferencia');
+INSERT INTO Metodo(Metodo) VALUES ('Cheque');
+INSERT INTO Metodo(Metodo) VALUES ('Efectivo');
 
+-- Vista
+CREATE VIEW TipEmp
+AS SELECT ID AS clave, NombreTipo AS NT 
+FROM TipoEmpleado;
 
- /*
- SELECT imagenEmpleado FROM Empleado;
- 
- SELECT * FROM Empleado;
- 
- 
- DELETE FROM Empleado ORDER BY ID DESC LIMIT 1;
- 
- SELECT * FROM Empleado;
- SELECT * FROM Telefonos;
- SELECT * FROM RedesSociales;
- SELECT * FROM Empleado_TipoEmpleado;
- 
- SELECT * FROM TipoEmpleado;
- 
- SELECT tp.NombreTipo FROM TipoEmpleado AS tp
- INNER JOIN Empleado_TipoEmpleado AS tpe ON(tpe.TipoEmpleadoID = tp.ID)
- INNER JOIN Empleado AS e ON(tpe.EmpleadoID = e.ID)
- WHERE e.ID = 59;
- 
- UPDATE Empleado
- SET
-	Nombre = "Lel",
-    Calle = "lol"
- WHERE ID = 59;   
- 
- 
- SELECT * FROM Empleado;
- SELECT * FROM Telefonos;
- SELECT * FROM RedesSociales;
- 
- SELECT tr.NombreRedSocial, r.Cuenta FROM TipoDeRedSocial as tr
- INNER JOIN RedesSociales AS r ON(r.TipoDeRedSocialID = tr.ID)
- INNER JOIN Empleado AS emp ON(emp.ID = r.EmpleadoID)
- WHERE emp.ID = 59;
- 
- SELECT ID FROM TipoDeRedSocial WHERE NombreRedSocial = 'Facebook';
- 
- SELECT tr.ID FROM TipoDeRedSocial AS tr
- INNER JOIN RedesSociales AS r ON(tr.ID = r.TipoDeRedSocialID)
- WHERE tr.NombreRedSocial = 'Instagram';
- 
- INSERT INTO RedesSociales
- VALUES(61,'hvg',1);
- 
- SELECT * FROM TipoDeRedSocial;
+-- Consultas
+SELECT * FROM Telefonos;
+SELECT * FROM Empleado;
+SELECT * FROM RedesSociales;
+SELECT * FROM Sesiones;
+SELECT * FROM Empleado;
 
-DELETE FROM Empleado_TipoEmpleado WHERE EmpleadoID = 40;
-
-DELETE FROM RedesSociales WHERE EmpleadoID = 59;
-
-DELETE FROM RedesSociales 
-WHERE EmpleadoID = (SELECT MAX(ID) FROM Empleado);
-
-DELETE FROM Telefonos 
-WHERE EmpleadoID = (SELECT MAX(ID) FROM Empleado);
-
--- DROP DATABASE editorialpiedra; */
